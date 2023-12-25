5961861e074c2d04c6ec0061337b441e7f770924
RojuGhimire <ghimireroju@gmail.com>
2023-12-25 19:55:39 +0545

    made login page successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-
-import React,{useState} from 'react'
-
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
- const [ username, setusername]= useState('')
- const [ password, setpassword]= useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate email (simple check for @ and .com)
+    if (!email.includes('@') || !email.includes('.com')) {
+      alert('Invalid email format');
+      return;
+    }
+
+    // Your login logic here
+
+    // Clear the form fields
+    setEmail('');
+    setPassword('');
+  };
 
   return (
-    
-    <div className="flex flex-col justify-center mt-40 items-center ">
-       <form Submit="">
-        <h1 className='text-3xl'>Login</h1>
-      <div className="m-4">
-        <label htmlFor="userame">UserName</label>
-        <input value ={username} id='username' type='text' />
-      </div>
-      <div className="m-4">
-        <label htmlFor="userame">Password</label>
-        <input className='border border-black round-md' value ={password} id='password' type='text' />
-      </div>
-      <div className="flex justify center">
-      <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-  Login
-</button>
-
-      </div>
-      
-       </form>
+    <div className="flex flex-col justify-center mt-40 items-center">
+      <form onSubmit={handleSubmit}>
+        <h1 className="text-3xl">Login</h1>
+        <div className="m-4">
+          <label htmlFor="username">Email</label>
+          <input
+            value={email}
+            onChange={handleEmailChange}
+            id="email"
+            type="text"
+          />
+        </div>
+        <div className="m-4">
+          <label htmlFor="password">Password</label>
+          <input
+            className="border border-black round-md"
+            value={password}
+            onChange={handlePasswordChange}
+            id="password"
+            type="password" // Change to password type for hiding characters
+          />
+        </div>
+        <div className="flex justify-center">
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Login
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
